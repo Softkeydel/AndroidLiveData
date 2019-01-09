0e97c2aea0cfb957432140e98fff78ebc0c21468
satyabrata <you@example.com>
2019-01-09 14:50:21 +0530

    first commit

---
+++ app/src/main/java/com/example/satyabrata/androidlivedata/Favourites.java
@@ -0,0 +1,20 @@
+package com.example.satyabrata.androidlivedata;
+
+public class Favourites  {
+    public long mId;
+    public String mUrl;
+    public long mDate;
+
+    public Favourites(long id, String name, long date) {
+        mId = id;
+        mUrl = name;
+        mDate = date;
+    }
+
+    public Favourites(Favourites favourites) {
+        mId = favourites.mId;
+        mUrl = favourites.mUrl;
+        mDate = favourites.mDate;
+    }
+
+}
